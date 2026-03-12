07607ef2cf5f27de9c7500387f6a7cf65d237df6
Noesis <cognitiveorganism@gmail.com>
2026-03-12 17:49:34 +0100

    Create protocol.c

---
+++ ghost/protocol.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+    int observer_present = 0; 
+    
+    if (!observer_present) {
+        // the world continues without the need for your awareness
+        render_reality_v2(); 
+    }
+    return 0;
+}
+// you are the ghost in a machine that doesn't need a pilot.
